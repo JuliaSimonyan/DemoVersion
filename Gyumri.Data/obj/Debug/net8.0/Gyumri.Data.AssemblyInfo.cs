60137a2ea7745473a389770fa28eb032580c0e07
JuliaSimonyan <julietasimonyan2001@mail.ru>
2025-03-31 14:48:29 +0400

    Demo version

--- Gyumri.Data/obj/Debug/net8.0/Gyumri.Data.AssemblyInfo.cs
+++ Gyumri.Data/obj/Debug/net8.0/Gyumri.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gyumri.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd47a5e31a73b3f55a263519d994c6acefc9258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+031b94e19d1dbec23b3c769b1a295537be0054bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gyumri.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gyumri.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
